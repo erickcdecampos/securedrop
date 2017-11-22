970183c9e09661861b96c9037f5e03815135a547
Mickael E <mickael@freedom.press>
2017-11-22 17:55:57 -0500

    WIP: There appears to be a (major) regression: Tor daemon doesnt start, but now the shortcut works. Modified SecureDrop init script to modify .desktop file metadata

--- install_files/ansible-base/roles/tails-config/files/securedrop_init.py
+++ install_files/ansible-base/roles/tails-config/files/securedrop_init.py
@@ -48,7 +48,32 @@ subprocess.call(['/usr/bin/dconf', 'write',
                  '/org/gnome/nautilus/preferences/automatic-decompression',
                  'false'])
 
-# notify the user
+# Set journalist.desktop and source.desktop links as trusted with Nautilus (see
+# https://github.com/freedomofpress/securedrop/issues/2586)
+#Set environment variables and euid to amnesia user
+uid=int(os.getenv("SUDO_UID"))
+gid=int(os.getenv("SUDO_GID"))
+home = os.getenv("HOME")
+logname = os.getenv("LOGNAME")
+os.environ["XDG_RUNTIME_DIR"] = "/run/user/1000"
+os.environ["XDG_DATA_DIRS"] = "/usr/share/gnome:/usr/local/share/:/usr/share/"
+os.environ["HOME"]="/home/amnesia"
+os.environ["LOGNAME"]="amnesia"
+os.setresgid(gid, gid, -1)
+os.setresuid(uid, uid, -1)
+subprocess.call(['rm', '/home/amnesia/Desktop/journalist.desktop'])
+subprocess.call(['rm', '/home/amnesia/Desktop/source.desktop'])
+subprocess.call(['ln', '-s', '/lib/live/mount/persistence/TailsData_unlocked/dotfiles/Desktop/journalist.desktop', '/home/amnesia/Desktop/journalist.desktop'])
+subprocess.call(['ln', '-s', '/lib/live/mount/persistence/TailsData_unlocked/dotfiles/Desktop/source.desktop', '/home/amnesia/Desktop/source.desktop'])
+subprocess.call(['gio', 'set', '/home/amnesia/Desktop/journalist.desktop', 'metadata::trusted', 'yes'])
+subprocess.call(['gio', 'set', '/home/amnesia/Desktop/source.desktop', 'metadata::trusted', 'yes'])
+
+os.environ["HOME"] = home
+os.environ["LOGNAME"] = logname
+
+# reacquire uid0 and notify the user
+os.setresuid(0,0,-1)
+os.setresgid(0,0,-1)
 subprocess.call(['tails-notify-user',
                  'SecureDrop successfully auto-configured!',
                  'You can now access the Journalist Interface.\nIf you are an admin, you can now SSH to the servers.'])
